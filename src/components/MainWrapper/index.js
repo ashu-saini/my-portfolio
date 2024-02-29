e025f59f22db148cdb277f967ceef1f8169bcf51
ashu.saini@bigsteptech.com-at-460015929520 <asaini@social27.com>
2024-02-29 16:31:32 +0530

    remove usestyles

--- src/components/MainWrapper/index.js
+++ src/components/MainWrapper/index.js
@@ -1,30 +1,16 @@
 import React from "react"
-import { makeStyles } from "@mui/styles";
 import { Box } from "@mui/material";
 import Container from 'components/Container'
 import Experiences  from 'components/MainWrapper/Experience'
 import Skills  from 'components/MainWrapper/Skills'
 import Projects  from 'components/MainWrapper/Projects'
 import Contact  from 'components/MainWrapper/Contact'
-
-const useStyles = makeStyles((theme) => ({
-  container: {
-    width: '100%',
-    paddingTop: '60px',
-    paddingBottom: '60px',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    gap: '2rem'
-  }
-}))
+import './index.scss'
 
 function MainWrapper() {
-  const classes = useStyles();
   return (
     <Container>
-      <Box className={classes.container}>
+      <Box className='main-wrapper'>
         <Experiences />
         <Skills />
         <Projects />
