1b35104e9960ef3a4a7efbebd8d0f6d1c3a703e0
gaurangkeluskar22 <Gaurang.Keluskar@ril.com>
2024-02-29 17:44:43 +0530

    added svgs for skills

--- src/components/MainWrapper/Skills/constant.js
+++ src/components/MainWrapper/Skills/constant.js
@@ -1,19 +1,52 @@
 import UiImg from '../../../assets/designer.png'
 import BackendImg from '../../../assets/web-development.png'
 import ToolsImg from '../../../assets/technical-support.png'
+import ReactImg from 'assets/svg/react.svg'
+import AngularImg from 'assets/svg/angular.svg'
+import JavascriptImg from 'assets/svg/javascript.svg'
+import TypescriptImg from 'assets/svg/typescript.svg'
+import WebpackImg from 'assets/svg/webpack.svg'
+import HtmlImg from 'assets/svg/html5.svg'
+import NodeImg from 'assets/svg/nodejs.svg'
+import SQLImg from 'assets/svg/mysql.svg' 
+import MongoDbImg from 'assets/svg/mongodb.svg'
+import AwsImg from 'assets/svg/aws.svg'
+import DockerImg from 'assets/svg/docker.svg'
+import ChatImg from 'assets/svg/chat.svg'
+import WebrtcImg from 'assets/svg/webrtc.svg'
+import SdkImg from 'assets/svg/sdkImg.svg'
+import ExpressImg from 'assets/svg/expressImg.png'
+import MysqlImg from 'assets/svg/sql.svg'
+
 export const skills = [
     {   icon: UiImg,
         title: 'Frontend',
-        key: ['React/Redux/React Hooks/Context API', 'Angular', 'Javascript', 'Typescript', 'Webpack', 'HTML/CSS/SASS']
+        key: [
+            {languague:'React/Redux/React Hooks/Context API', image:ReactImg}, 
+            {languague:'Angular', image:AngularImg}, 
+            {languague:'Javascript', image:JavascriptImg}, 
+            {languague:'Typescript', image:TypescriptImg}, 
+            {languague:'Webpack', image:WebpackImg}, 
+            {languague:'HTML/CSS/SASS', image:HtmlImg}]
 
     },
     {   icon: BackendImg,
         title: 'Backend',
-        key: ['Node.js/Express', 'PostgresSQL/MySQL', 'MongoDB']
+        key: [
+            {languague:'Node.js', image:NodeImg}, 
+            {languague:'Express', image:ExpressImg},
+            {languague:'PostgresSQL', image:SQLImg}, 
+            {languague:'MySQL', image:MysqlImg},
+            {languague:'MongoDB', image:MongoDbImg}]
     },
     {   icon: ToolsImg,
         title: 'Tools',
-        key: ['AWS', 'Docker', 'WebRTC', 'RTC SDK', 'Chat SDK']
+        key: [
+            {languague:'AWS', image:AwsImg}, 
+            {languague:'Docker', image:DockerImg}, 
+            {languague:'WebRTC', image:WebrtcImg}, 
+            {languague:'RTC SDK', image:SdkImg}, 
+            {languague: 'Chat SDK', image:ChatImg}]
     }
 ]
 
