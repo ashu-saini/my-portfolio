8c969a029c6a02cabcbc3291334913d32210d0ef
ashu.saini@bigsteptech.com-at-460015929520 <asaini@social27.com>
2024-03-01 15:11:20 +0530

    Updates

--- src/components/ContactInfo/constant.js
+++
@@ -1,5 +0,0 @@
-export const contacts = {
-  email: "ashusaini542@gmail.com",
-  phone: "+917836045022",
-  website: "https://ashu-saini.github.io/my-portfolio",
-}
--- src/components/MainWrapper/Contact/constant.js
+++
@@ -1,6 +0,0 @@
-export const info = {
-  title: "Interested in hiring me for your project?",
-  email: 'example@gmail.com',
-  phone: '+123456789',
-  alt: 'Example'
-}
