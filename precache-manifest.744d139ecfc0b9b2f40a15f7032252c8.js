e8b740ba02456aee41018d9ad49105f29ce90af8
ashu.saini@bigsteptech.com-at-460015929520 <asaini@social27.com>
2024-06-03 09:54:52 +0530

    Updates

---
+++ precache-manifest.744d139ecfc0b9b2f40a15f7032252c8.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85ab5987e849e6bbeafd1a4852b901b2",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "195477f9c15693bb01bf",
+    "url": "/my-portfolio/static/css/main.dc633f74.chunk.css"
+  },
+  {
+    "revision": "495a94e4ed958d78bf57",
+    "url": "/my-portfolio/static/js/2.eafb337c.chunk.js"
+  },
+  {
+    "revision": "5a1853fc6935e23e3792872d18da39b1",
+    "url": "/my-portfolio/static/js/2.eafb337c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "195477f9c15693bb01bf",
+    "url": "/my-portfolio/static/js/main.ae987495.chunk.js"
+  },
+  {
+    "revision": "f8bc21605bac90413ed8",
+    "url": "/my-portfolio/static/js/runtime-main.cc7e5888.js"
+  },
+  {
+    "revision": "258400b553199ae47b51eaa5c0141518",
+    "url": "/my-portfolio/static/media/Ashu_Resume.258400b5.pdf"
+  },
+  {
+    "revision": "093ee89be9ede30383f39a899c485a82",
+    "url": "/my-portfolio/static/media/Poppins-Regular.093ee89b.ttf"
+  },
+  {
+    "revision": "462b179aec03d3a750157090ca181364",
+    "url": "/my-portfolio/static/media/angular.462b179a.svg"
+  },
+  {
+    "revision": "e626e0ffb032a2f6fbef879e5548a767",
+    "url": "/my-portfolio/static/media/aws.e626e0ff.svg"
+  },
+  {
+    "revision": "0b43d0b1c37d531363050cbb65060257",
+    "url": "/my-portfolio/static/media/chat.0b43d0b1.svg"
+  },
+  {
+    "revision": "1ced4ee629fa2342c943436be86117ef",
+    "url": "/my-portfolio/static/media/designer.1ced4ee6.png"
+  },
+  {
+    "revision": "1e9804ca2dc56c1e0cbb59f03b4496ae",
+    "url": "/my-portfolio/static/media/docker.1e9804ca.svg"
+  },
+  {
+    "revision": "458c1a2be2f1b78f3ce6e3dc142f7aa1",
+    "url": "/my-portfolio/static/media/expressImg.458c1a2b.png"
+  },
+  {
+    "revision": "4447440d22b307c33a57c729e7d7c599",
+    "url": "/my-portfolio/static/media/html5.4447440d.svg"
+  },
+  {
+    "revision": "61deeb1e557dc853b214794ca7aac6c5",
+    "url": "/my-portfolio/static/media/javascript.61deeb1e.svg"
+  },
+  {
+    "revision": "81aa65e37d283d071fdbd93d605e5c3d",
+    "url": "/my-portfolio/static/media/linkedIn.81aa65e3.svg"
+  },
+  {
+    "revision": "ebf723d47dc4203868088d005452543b",
+    "url": "/my-portfolio/static/media/mongodb.ebf723d4.svg"
+  },
+  {
+    "revision": "2dbaa7138d55b33c09f813514ab1b9da",
+    "url": "/my-portfolio/static/media/mysql.2dbaa713.svg"
+  },
+  {
+    "revision": "526340ef8a505710f941759e6831b796",
+    "url": "/my-portfolio/static/media/nodejs.526340ef.svg"
+  },
+  {
+    "revision": "236dc2aff5840583d78311d29a196c31",
+    "url": "/my-portfolio/static/media/profile.236dc2af.jpg"
+  },
+  {
+    "revision": "21844418646f368e3b8cc9a481d20c65",
+    "url": "/my-portfolio/static/media/react.21844418.svg"
+  },
+  {
+    "revision": "d15c0c080d81cad231ec06c9d5900b22",
+    "url": "/my-portfolio/static/media/sdkImg.d15c0c08.svg"
+  },
+  {
+    "revision": "c8b715be80e9253c0e46515fae76b7ff",
+    "url": "/my-portfolio/static/media/skype.c8b715be.svg"
+  },
+  {
+    "revision": "8b22fe7d840ee6daf9b4bfaa5e0f107d",
+    "url": "/my-portfolio/static/media/sql.8b22fe7d.svg"
+  },
+  {
+    "revision": "35c19ad75f04b0e4de503c5ee282bc29",
+    "url": "/my-portfolio/static/media/technical-support.35c19ad7.png"
+  },
+  {
+    "revision": "5466068bce2f445414bd579cbd6c5c98",
+    "url": "/my-portfolio/static/media/typescript.5466068b.svg"
+  },
+  {
+    "revision": "588924ff89cc933e11ee0ec5911a61cc",
+    "url": "/my-portfolio/static/media/web-development.588924ff.png"
+  },
+  {
+    "revision": "41d5c40fd56680880712de7863af1e31",
+    "url": "/my-portfolio/static/media/webpack.41d5c40f.svg"
+  },
+  {
+    "revision": "a9cb81af44e0597900b5c001392c524c",
+    "url": "/my-portfolio/static/media/webrtc.a9cb81af.svg"
+  }
+]);
